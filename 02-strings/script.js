d42f8befa8326b894f33b88bf043a301a9119d97
erikaCutugno <erikucciapop@gmail.com>
2024-11-27 15:46:03 +0100

    fix: exe2

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -8,8 +8,11 @@ console.log(name)
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
 const number = 22;
+console.log(number)
 const text = "ciao";
+console.log(text)
 const bool = true;
+console.log(bool)
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
